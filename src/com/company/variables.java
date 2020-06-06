94f900a9e6d54666cd968b4dcedea0ac18710204
Mahinder P S Narang <mpsnarang@gmail.com>
2020-06-06 12:46:53 +1000

    first change

---
+++ src/com/company/variables.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class variables {
+        public static void main(String[] args)
+        {
+            // comment here...
+
+            // declaring variables
+            int marks1;
+            int marks2;
+
+
+            // assigning values & initialization of variables
+            marks1 = 72;
+            marks2 = 85;
+
+            // calculating total of marks
+            double multiply = marks1 * marks2;
+
+            // declaring variables in a single line
+            String student1 = "Adam";
+            String student2 = "Lucy";
+
+            // printing values of marks and student names
+            System.out.println(multiply);
+            System.out.println(student2);
+
+        }
+    }
+
+
